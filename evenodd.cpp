d03f99c93f9bb39706f56691a48ad37406db34ff
Siyon Kurane <siyonkurane@gmail.com>
2026-07-21 08:38:25 +0000

    change

--- evenodd.cpp
+++ evenodd.cpp
@@ -5,9 +5,13 @@ int main()
     int a;
     cout<<"Enter a number:";
     cin>>a;
-    if (a)%2==0:
-    cout<<"The number is even"
-
-    else:
-    cout<<"The number is odd"
+    if (a%2==0)
+    {
+      cout<<"The number is even";
+    }
+    else
+    {
+      cout<<"The number is odd";
+    }
+    return 0;
 }
