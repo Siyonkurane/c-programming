585ff4432afd0b9c1e70aab0618164fffbfa544b
Siyon Kurane <siyonkurane@gmail.com>
2026-07-21 08:58:11 +0000

    runned

---
+++ LOTN.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main
+{
+    int a=10,b=20,c=15;
+    if (a>b && a>c)
+    {
+        cout<<"a is Greatest";
+    }
+    elif (b>c)
+    {
+        cout
+    }
+}
---
+++ LargestOfTwoNo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=40;
+    if (a>b)
+    {
+        cout<<"a is greatest";
+    }
+    else
+    {
+        cout<<"b is greatest";
+    }
+    return 0;
+}
