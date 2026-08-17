43a27f7f9f9b5e49575899c16f5f6b7339ad25a1
Siyon Kurane <siyonkurane@gmail.com>
2026-08-17 14:13:14 +0000

    Error (Use of getline or setprecission)

---
+++ EmployeeInfo.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+    int employeeID;
+    string employeeName;
+    string department;
+    float annualSalary;
+
+public:
+    void accept()
+    {
+        cout << "Enter Employee ID: ";
+        cin >> employeeID;
+
+        cout << "Enter Employee Name: ";
+        cin >> employeeName;
+
+        cout << "Enter Department: ";
+        cin >> department;
+
+        cout << "Enter Annual Salary: ";
+        cin >> annualSalary;
+    }
+
+    void display()
+    {
+        cout << "Employee Information :- "<< endl;
+        cout << "Employee ID: " << employeeID << endl;
+        cout << "Employee Name: " << employeeName << endl;
+        cout << "Department: " << department << endl;
+        cout << "Annual Salary: " << annualSalary << endl;
+    }
+};
+
+int main()
+{
+    Employee e;
+
+    e.accept();
+    e.display();
+
+    return 0;
+}
