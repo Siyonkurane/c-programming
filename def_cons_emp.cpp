f79cb373b8d54dfc641c95367a9e1bec474b3c09
Siyon Kurane <siyonkurane@gmail.com>
2026-08-25 08:50:48 +0000

    done

---
+++ def_cons_emp.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+    string name;
+    int id;
+    float salary;
+
+public:
+    
+    Employee()
+    {
+        name = "Siyon";
+        id = 101;
+        salary = 50000;
+    }
+
+    void display()
+    {
+        cout << "Employee Name: " << name << endl;
+        cout << "Employee ID: " << id << endl;
+        cout << "Employee Salary: " << salary << endl;
+    }
+};
+
+int main()
+{
+    Employee e;   
+
+    e.display();
+
+    return 0;
+}
